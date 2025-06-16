e59fd8311d3033568de2879a41acff070c641cd2
Cristian Alexander Sierra <159510185+Csierra036@users.noreply.github.com>
2025-06-16 11:47:22 -0400

    feat: develop (#5)
    * feat: login endpoints services
    
    * feat: JWT services
    
    * update: gitignore
    
    * Feature/jwt login service (#4)
    
    * feat: login endpoints services
    
    * feat: JWT services
    
    * remove: app.py
    
    * feat: env.example
    
    * update: database and env.example

---
+++ backend/src/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from src.config import settings
+
+if settings.ENV is 'production':
+    DATABASE_URL = f"postgresql://postgres.nqsrgdbrgwvhkijvdjps:{settings.DB_PASSWORD}@aws-0-us-east-2.pooler.supabase.com:6543/postgres"
+else:
+    DATABASE_URL = f"postgresql+psycopg2://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+    
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
