5aa8b318e50a47a71f9119303c83652c1bc99e2c
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-06-16 12:30:31 -0400

    fix: database production

--- backend/src/database.py
+++ backend/src/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from src.config import settings
 
-if settings.ENV is 'production':
-    DATABASE_URL = f"postgresql://postgres.nqsrgdbrgwvhkijvdjps:{settings.DB_PASSWORD}@aws-0-us-east-2.pooler.supabase.com:6543/postgres"
+if settings.ENV == 'production':
+    DATABASE_URL = f"postgresql://postgres.nqsrgdbrgwvhkijvdjps:{settings.DB_PASSWORD}@aws-0-us-east-2.pooler.supabase.com:5432/postgres"
 else:
     DATABASE_URL = f"postgresql+psycopg2://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
-    
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
