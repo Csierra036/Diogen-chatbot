edb9e1c68c12037c607ca3f252ffc9b2e7bf2108
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-06-16 12:48:53 -0400

    feat: update envs

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
-
+from typing import Optional
 class Settings(BaseSettings):
     GOOGLE_API_KEY: str
-    DB_USERNAME: str
+    DB_USERNAME: Optional[str]
     DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: str
-    DB_NAME: str
+    DB_HOST: Optional[str]
+    DB_PORT: Optional[str]
+    DB_NAME: Optional[str]
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
